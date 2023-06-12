f4a5deffa951ed06dcaff89f4dabbf30b17d9a83
MarvanT <marvan.t98@gmail.com>
2023-06-12 15:57:10 +0100

    Setting up the DB

---
+++ Data/DataContext.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnet_rpg.Data
+{
+    /*
+      You can use an instance of DbContext to interact with the database (query)
+    */
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            
+        }
+
+        /* 
+        Name of the DbSet is the name of the corresponding database table. 
+        When you need a representation of model in the database, you have to add a DbSet of the model.
+
+        This is how it knows what tables that it should create 
+        */
+        //a property with only a getter, which uses the Set<TEntity> method to get a DbSet for Character entities.
+        public DbSet<Character> Characters => Set<Character>(); 
+        
+    }   
+}
--- Program.cs
+++ Program.cs
@@ -2,10 +2,18 @@ global using dotnet_rpg.Models; //this is visible everywhere (first thing before
 global using dotnet_rpg.Services.CharacterService;
 global using dotnet_rpg.Dtos.Character;
 global using AutoMapper;
+global using Microsoft.EntityFrameworkCore;
+global using dotnet_rpg.Data;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+// DB
+// The lambda expression configures our context to connect to SQLServer DB, this method takes the ConnectionString
+builder.Services.AddDbContext<DataContext>(options =>
+ options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))); // This is why the convetion "ConnectionStrings" makes sense
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
