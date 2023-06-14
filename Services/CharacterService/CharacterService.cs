8c120c0d42ea50e5065df97353ae607bc1353695
MarvanT <marvan.t98@gmail.com>
2023-06-14 18:56:09 +0100

    Delete mapping updated to interact with the DB

--- Services/CharacterService/CharacterService.cs
+++ Services/CharacterService/CharacterService.cs
@@ -8,11 +8,6 @@ namespace dotnet_rpg.Services.CharacterService
 
     public class CharacterService : ICharacterService
     {
-        private static List<Character> characters = new List<Character> {
-            new Character(),
-            new Character { Id = 1,  Name = "Sam" }
-        };
-
         private readonly IMapper _mapper;
         private readonly DataContext _context;
     
@@ -100,10 +95,12 @@ namespace dotnet_rpg.Services.CharacterService
             var serviceResponse = new ServiceResponse<List<GetCharacterResponseDto>>();
 
             try {
-                var character = characters.FirstOrDefault(c => c.Id == id) ?? throw new Exception($"Character with id: {id} not found"); // First can be used but it returns an exception when the character is not found
+                var character = await _context.Characters.FirstOrDefaultAsync(c => c.Id == id) ?? throw new Exception($"Character with id: {id} not found"); // First can be used but it returns an exception when the character is not found
 
-                characters.Remove(character);
-                serviceResponse.Data = characters.Select(c => _mapper.Map<GetCharacterResponseDto>(c)).ToList();
+                _context.Characters.Remove(character);
+                await _context.SaveChangesAsync();
+
+                serviceResponse.Data = await _context.Characters.Select(c => _mapper.Map<GetCharacterResponseDto>(c)).ToListAsync();
 
             } catch (Exception e) {
                 serviceResponse.Success = false;
