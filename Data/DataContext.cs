522ba2212d93569755b67618a763aa3c7ade48a8
MarvanT <marvan.t98@gmail.com>
2023-08-24 17:25:42 +0100

    Add use of Entity filters and data seeding in Entity Framework Core
    This commit introduces two important topics related to Entity Framework Core usage - Entity or Query filters and Data Seeding. Two separate comprehensive explanation files are added, one for each topic.
    
    Entity filters are a powerful tool that includes filtering logic directly on the Db Context, while Data Seeding allows pre-populating the database with necessary values, examples are given for both.
    
    The DataContext.cs file is also modified to include data seeding directly into the model builder, providing initial Skill data in the database.
    
    The migration files related to data seeding are also included in this commit.
    
    In addition to explaining these concepts, this change provides a useful reference for their implementation in future projects.

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -9,7 +9,16 @@ public class DataContext : DbContext
     {
             
     }
-    
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Skill>().HasData(
+            new Skill { Id = 1, Name = "Fireball", Damage = 5 },
+            new Skill { Id = 2, Name = "Thunderbolt", Damage = 8 },
+            new Skill { Id = 3, Name = "Ice Shard", Damage = 7 }
+        );
+    }
+
     public DbSet<Character> Characters => Set<Character>(); 
     public DbSet<User> Users => Set<User>();
     public DbSet<Weapon> Weapons => Set<Weapon>();
