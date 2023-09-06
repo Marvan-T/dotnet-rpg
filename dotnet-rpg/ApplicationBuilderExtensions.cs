7ed92130496f81c9341f447e75e0b7bee73cb348
MarvanT <marvan.t98@gmail.com>
2023-09-06 16:43:48 +0100

    Refactor application setup logic and log level enhancement
    The changes made in the commit majorly revolve around refactoring and enhancing the system for better clarity and control. Firstly, the configuration of services in the main program file has been encapsulated into their respective extension methods for easier management, better segregation of concerns reducing clutter and promoting code reusability. Secondly, the log level for 'Microsoft.AspNetCore.Authentication' has been set to 'Information'. This will provide more detailed logging which is imperative for debugging the authentication process. Also, a global usages file has been added for project-wide name resolution which aids in cleaner code organization. The dotnet.yml file has only had the addition of newline for proper reconciliation with UNIX format.

---
+++ dotnet-rpg/ApplicationBuilderExtensions.cs
@@ -0,0 +1,82 @@
+using System.Text;
+
+namespace dotnet_rpg;
+
+public static class ApplicationBuilderExtensions
+{
+    //// The lambda expression configures our context to connect to SQLServer DB, this method takes the ConnectionString
+    public static void AddDefaultDbContext(this IServiceCollection services, string connectionString)
+    {
+        // DB context setup
+        services.AddDbContext<DataContext>(options =>
+            options.UseSqlServer(connectionString));
+    }
+
+    public static void AddJwt(this IServiceCollection services, IConfiguration configuration)
+    {
+        var jwtSigningKeyConf = configuration.GetSection("AppSettings:JwtSigningKey").Value;
+        Console.WriteLine($"Got JWT signing key from configuration: '{jwtSigningKeyConf}'");
+        // JWT setup
+        services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+        {
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey =
+                    new SymmetricSecurityKey(
+                        Encoding.UTF8.GetBytes(configuration.GetSection("AppSettings:JwtSigningKey").Value!)),
+                ValidateIssuer = false,
+                ValidateAudience = false
+            };
+        });
+    }
+
+    public static void AddSwagger(this IServiceCollection services)
+    {
+        // Swagger and SwaggerUI setup
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        services.AddSwaggerGen(c =>
+        {
+            c.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+            {
+                Description = """Standared Authorization header using the Bearer scheme. Example: "bearer {token}" """,
+                In = ParameterLocation.Header,
+                Name = "Authorization",
+                Type = SecuritySchemeType.ApiKey
+            });
+        });
+    }
+
+    public static void AddDefaultServices(this IServiceCollection services)
+    {
+        // Dependency injection setup
+        // Inject CharacterService when looking for ICharacterService
+        // Scoped - new instance of service is created for every request that comes in (stateful within the request)
+        // Transient - new instance every time
+        // Singleton - same instance always
+        services.AddScoped<ICharacterService, CharacterService>();
+
+        /* More on Scoped
+        Scoped is useful when you require a new instance for each user request, making it ideal for handling user-specific operations like authentication,
+        ensuring that the operations and data for one user do not interfere with those for another.
+
+        If AuthRepository uses a context like DbContext in Entity Framework, it should be registered as Scoped, because DbContext is not thread-safe.
+
+        However, the choice really depends on what exactly AuthRepository does. If it's stateless, you could even register it as a Singleton for better performance.
+        Be careful, though, as singletons can cause thread safety issues unless they are stateless or thread-safe.
+        */
+        services.AddScoped<IAuthRepository, AuthRepository>();
+        services.AddScoped<IAuthService, AuthService>();
+        services.AddScoped<IWeaponService, WeaponService>();
+        services.AddScoped<IRepository<Character>, CharacterRepository>();
+        services.AddScoped<IRepository<Weapon>, WeaponRepository>();
+        services.AddScoped<IRepository<Skill>, SkillRepository>();
+    }
+
+    public static void AddDefaultAutoMapper(this IServiceCollection services)
+    {
+        // Adds AutoMapper to the dependency injection container. 
+        // The typeof(Program).Assembly parameter specifies the assembly where the AutoMapper profiles are located.
+        services.AddAutoMapper(typeof(Program).Assembly);
+    }
+}
---
+++ dotnet-rpg/Usings.cs
@@ -0,0 +1,14 @@
+global using dotnet_rpg.Models;
+global using dotnet_rpg.Services.CharacterService;
+global using dotnet_rpg.Dtos.Character;
+global using dotnet_rpg.Dtos.Weapon;
+global using AutoMapper;
+global using Microsoft.EntityFrameworkCore;
+global using dotnet_rpg.Data;
+global using dotnet_rpg.Auth;
+global using dotnet_rpg.Services.WeaponService;
+global using Microsoft.AspNetCore.Authentication.JwtBearer;
+global using Microsoft.IdentityModel.Tokens;
+global using Microsoft.OpenApi.Models;
+global using dotnet_rpg.Repository;
+global using Swashbuckle.AspNetCore.Filters;
