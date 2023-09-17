15f72b2d97492fe1141d788ec14e836d536813a6
MarvanT <marvan.t98@gmail.com>
2023-09-17 14:49:05 +0100

    Refactor FightService to separate strategy execution
    Decoupled the execution of attack performance strategy from FightService to a new service AttackPerformService. This helps in adhering to Single Responsibility Principle where FightService is now responsible for handling different types of attacks and AttackPerformService takes care of how an attack is performed. This will help in improving modularity and make future extensions or changes in attack strategies easier.

---
+++ dotnet-rpg/Services/AttackPerformService/AttackPerformService.cs
@@ -0,0 +1,82 @@
+using dotnet_rpg.Dtos.Fight;
+using dotnet_rpg.Services.CharacterLookupService;
+
+namespace dotnet_rpg.Services.AttackPerformService;
+
+public class AttackPerformService : IAttackPerformService
+{
+    private readonly ICharacterLookupService _characterLookupService;
+    private readonly IRepository<Character> _characterRepository;
+
+    public AttackPerformService(ICharacterLookupService characterLookupService,
+        IRepository<Character> characterRepository)
+    {
+        _characterLookupService = characterLookupService;
+        _characterRepository = characterRepository;
+    }
+
+    public async Task<ServiceResponse<AttackResultDto>> PerformAttack(IAttackDto attackDto,
+        Func<Character, Character, int> attackStrategy)
+    {
+        var response = new ServiceResponse<AttackResultDto>();
+        try
+        {
+            var attacker = await _characterLookupService.FindCharacterByUserAndCharacterId(attackDto.AttackerId);
+            var opponent = await _characterLookupService.FindCharacterByCharacterId(attackDto.OpponentId);
+
+            if (IsDefeated(opponent)) return BuildDefeatedResponse(opponent, response);
+
+            var damageDealt = attackStrategy.Invoke(attacker, opponent);
+            UpdateFightStatistics(attacker, opponent);
+            await _characterRepository.SaveChangesAsync();
+
+            if (IsDefeated(opponent)) response.Message = $"{opponent.Name} has been defeated";
+
+            response.Data = BuildAttackResultDto(attacker, opponent, damageDealt);
+        }
+        catch (Exception e)
+        {
+            response.Success = false;
+            response.Message = e.Message;
+        }
+
+        return response;
+    }
+
+    private bool IsDefeated(Character character)
+    {
+        return character.HitPoints <= 0;
+    }
+
+    private ServiceResponse<AttackResultDto> BuildDefeatedResponse(Character opponent,
+        ServiceResponse<AttackResultDto> response)
+    {
+        response.Success = false;
+        response.Message = $"{opponent.Name} has already been defeated";
+        return response;
+    }
+
+    private void UpdateFightStatistics(Character attacker, Character opponent)
+    {
+        attacker.Fights += 1;
+        opponent.Fights += 1;
+
+        if (IsDefeated(opponent))
+        {
+            attacker.Victories += 1;
+            opponent.Defeats += 1;
+        }
+    }
+
+    private AttackResultDto BuildAttackResultDto(Character attacker, Character opponent, int damageDealt)
+    {
+        return new AttackResultDto
+        {
+            Attacker = attacker.Name,
+            Opponent = opponent.Name,
+            AttackerHp = attacker.HitPoints,
+            OpponentHp = opponent.HitPoints,
+            DamageDealt = damageDealt
+        };
+    }
+}
--- dotnet-rpg/Services/FightService/FightService.cs
+++ dotnet-rpg/Services/FightService/FightService.cs
@@ -1,73 +1,31 @@
 using dotnet_rpg.Dtos.Fight;
-using dotnet_rpg.Services.CharacterLookupService;
+using dotnet_rpg.Services.AttackPerformService;
 using dotnet_rpg.Utility.RandomGeneration;
 
 namespace dotnet_rpg.Services.FightService;
 
 public class FightService : IFightService
 {
-    private readonly ICharacterLookupService _characterLookupService;
-    private readonly IRepository<Character> _characterRepository;
+    private readonly IAttackPerformService _attackPerformService;
     private readonly IRandomGenerator _random;
 
-    public FightService(ICharacterLookupService characterLookupService, IRepository<Character> characterRepository,
+    public FightService(IAttackPerformService attackPerformService,
         IRandomGenerator random)
     {
-        _characterLookupService = characterLookupService;
-        _characterRepository = characterRepository;
+        _attackPerformService = attackPerformService;
         _random = random;
     }
 
     public async Task<ServiceResponse<AttackResultDto>> WeaponAttack(WeaponAttackDto weaponAttackDto)
     {
-        return await PerformAttack(weaponAttackDto, (attacker, opponent) => DoWeaponAttack(attacker, opponent));
+        return await _attackPerformService.PerformAttack(weaponAttackDto,
+            (attacker, opponent) => DoWeaponAttack(attacker, opponent));
     }
 
     public async Task<ServiceResponse<AttackResultDto>> SkillAttack(SkillAttackDto skillAttackDto)
     {
-        return await PerformAttack(skillAttackDto,
+        return await _attackPerformService.PerformAttack(skillAttackDto,
             (attacker, opponent) => DoSkillAttack(attacker, opponent, skillAttackDto.SkillId));
-    }
-
-    private async Task<ServiceResponse<AttackResultDto>> PerformAttack(IAttackDto attackDto,
-        Func<Character, Character, int> attackStrategy)
-    {
-        var response = new ServiceResponse<AttackResultDto>();
-        try
-        {
-            var attacker = await _characterLookupService.FindCharacterByUserAndCharacterId(attackDto.AttackerId);
-            var opponent = await _characterLookupService.FindCharacterByCharacterId(attackDto.OpponentId);
-
-            if (IsDefeated(opponent)) return BuildDefeatedResponse(opponent, response);
-
-            var damageDealt = attackStrategy.Invoke(attacker, opponent);
-            UpdateFightStatistics(attacker, opponent);
-            await _characterRepository.SaveChangesAsync();
-
-            if (IsDefeated(opponent)) response.Message = $"{opponent.Name} has been defeated";
-
-            response.Data = BuildAttackResultDto(attacker, opponent, damageDealt);
-        }
-        catch (Exception e)
-        {
-            response.Success = false;
-            response.Message = e.Message;
-        }
-
-        return response;
-    }
-
-    private bool IsDefeated(Character character)
-    {
-        return character.HitPoints <= 0;
-    }
-
-    private ServiceResponse<AttackResultDto> BuildDefeatedResponse(Character opponent,
-        ServiceResponse<AttackResultDto> response)
-    {
-        response.Success = false;
-        response.Message = $"{opponent.Name} has already been defeated";
-        return response;
     }
 
     private int DoWeaponAttack(Character attacker, Character opponent)
@@ -95,29 +53,5 @@ public class FightService : IFightService
         if (damage > 0) opponent.HitPoints -= damage;
 
         return damage;
-    }
-
-    private void UpdateFightStatistics(Character attacker, Character opponent)
-    {
-        attacker.Fights += 1;
-        opponent.Fights += 1;
-
-        if (IsDefeated(opponent))
-        {
-            attacker.Victories += 1;
-            opponent.Defeats += 1;
-        }
-    }
-
-    private AttackResultDto BuildAttackResultDto(Character attacker, Character opponent, int damageDealt)
-    {
-        return new AttackResultDto
-        {
-            Attacker = attacker.Name,
-            Opponent = opponent.Name,
-            AttackerHp = attacker.HitPoints,
-            OpponentHp = opponent.HitPoints,
-            DamageDealt = damageDealt
-        };
     }
 }
